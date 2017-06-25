443c4bac21a8f7f412eab27c0a1af28429315972
chesslav <vv-maslov@mail.ru>
2017-06-25 23:54:37 +0500

    Signed-off-by: chesslav <vv-maslov@mail.ru>

---
+++ TASK/minByte.f
@@ -0,0 +1,12 @@
+\ : SmMB
+\ 2/{:31b12GIb3XLd.c\}:4'm4X'M4X
+\ ;
+
+: SmMB 2/{:31b12GIb3XLd.c\}:4'm4X'M4X ;
+
+T: arr 5676686875534564trtuytughhjgkbvbnmbmb ;
+
+arr SmMB
+
+MaxByte: u! a! 0 a u  /G I /b MAX /L ;
+arr MaxByte
